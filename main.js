996975f3bddd76006da8174e44aa5c846964f1a7
Karthick Madhesh <karthickmathesh1995@gmail.com>
2022-08-11 14:24:20 +0530

    Todo List Code Updated

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-if('serviceWorker' in navigator){
+if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/sw.js')
-			.then(reg => {
-				console.log('Registered! ✅', reg);
-			}).catch(err =>{
-				console.log('⛔ Registration failed: ', err);
-			});
-	});
+		navigator.serviceWorker
+			.register('/sw.js')
+			.then((reg) => {
+				console.log('Registered! ✅', reg)
+			})
+			.catch((err) => {
+				console.log('⛔ Registration failed: ', err)
+			})
+	})
 }
